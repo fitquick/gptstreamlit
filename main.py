e8c792b8e0920d22fdc8d6898846aae2ea350b35
fitquick <141992941+fitquick@users.noreply.github.com>
2024-02-01 19:20:20 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,92 +1,137 @@
-import os
 import openai
 import streamlit as st
+from google.cloud import firestore
 
 # OpenAI APIキーの設定
 openai.api_key = st.secrets["OpenAIAPI"]["openai_api_key"]
 
+# Firestore設定（環境変数からGCPプロジェクトIDを取得）
+GCP_PROJECT = st.secrets["GCP"]["project_id"]
+db = firestore.Client(project=GCP_PROJECT)
 
-st.title("QUICKFIT BOT")
-st.write("Quick fitに関するQ&A AIBOT")
+MODEL_NAME = st.secrets["MODEL_NAME"]["Name"]
+MODEL_TEMPERATURE = st.secrets["MODEL_TEMPERATURE"]["TEMPERATURE"]
 
-HIDE_ST_STYLE = """
-                <style>
-                div[data-testid="stToolbar"] {
-                visibility: hidden;
-                height: 0%;
-                position: fixed;
+
+NEW_CHAT_TITLE = "New Chat"
+CHATBOT_USER = "QUIUK"
+GCP_PROJECT = "YOUR_GCP_PROJECT"
+
+def create_new_chat():
+    st.session_state.displayed_chat_title = NEW_CHAT_TITLE
+    st.session_state.displayed_chat_messages = []
+
+
+def change_displayed_chat(chat_doc):
+    # Update titles
+    st.session_state.titles = [
+        doc.to_dict()["title"] for doc in st.session_state.chats_ref.order_by("created").stream()
+    ]
+
+    st.session_state.displayed_chat_ref = chat_doc.reference
+    st.session_state.displayed_chat_title = chat_doc.to_dict()["title"]
+    st.session_state.displayed_chat_messages = [
+        msg.to_dict()
+        for msg in chat_doc.reference.collection("messages").order_by("timestamp").stream()
+    ]
+
+
+def run():
+
+    if "user" not in st.session_state:
+        st.session_state.user = CHATBOT_USER
+
+    if "chats_ref" not in st.session_state:
+        db = firestore.Client(project=GCP_PROJECT)
+        user_ref = db.collection("users").document(st.session_state.user)
+        st.session_state.chats_ref = user_ref.collection("chats")
+
+    if "titles" not in st.session_state:
+        st.session_state.titles = [
+                doc.to_dict()["title"]
+                for doc in st.session_state.chats_ref.order_by("created").stream()
+                ]
+
+    if "displayed_chat_ref" not in st.session_state:
+        st.session_state.displayed_chat_ref = None
+
+    if "displayed_chat_title" not in st.session_state:
+        st.session_state.displayed_chat_title = "New Chat"
+
+    if "displayed_chat_messages" not in st.session_state:
+        st.session_state.displayed_chat_messages = []
+
+    # Sidebar
+    with st.sidebar:
+        new_chat_disable = st.session_state.displayed_chat_title == NEW_CHAT_TITLE
+        st.button("新しい会話を始める", on_click=create_new_chat, disabled=new_chat_disable, type="primary")
+        st.title("過去の会話履歴")
+        for doc in st.session_state.chats_ref.order_by("created").stream():
+            data = doc.to_dict()
+            st.button(data["title"], on_click=change_displayed_chat, args=(doc, ))
+
+    # Display messages
+    for message in st.session_state.displayed_chat_messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if user_input_text := st.chat_input("質問を入力してください"):
+
+        # User
+        with st.chat_message("user"):
+            st.markdown(user_input_text)
+
+        # Process first message
+        if len(st.session_state.displayed_chat_messages) == 0:
+
+            # Create new chat title
+            chat_title_prompt = f"""
+            あなたは優秀なアシスタントです。ユーザーの質問に最高品質で答えなさい。"""
+
+            response = openai.ChatCompletion.create(
+                model=MODEL_NAME,
+                messages=[{'role': 'system', 'content': chat_title_prompt}]
+            )
+            st.session_state.displayed_chat_title = response['choices'][0]['message']['content']
+
+            # Create new chat on firestore
+            _, st.session_state.displayed_chat_ref = st.session_state.chats_ref.add(
+                {
+                'title': st.session_state.displayed_chat_title,
+                'created': firestore.SERVER_TIMESTAMP,
                 }
-                div[data-testid="stDecoration"] {
-                visibility: hidden;
-                height: 0%;
-                position: fixed;
-                }
-				        
-                </style>
-"""
+            )
 
-st.markdown(HIDE_ST_STYLE, unsafe_allow_html=True)
+        user_input_data = {
+            "role": "user",
+            "content": user_input_text,
+            "timestamp": firestore.SERVER_TIMESTAMP
+        }
+        st.session_state.displayed_chat_messages.append(user_input_data)
+        st.session_state.displayed_chat_ref.collection("messages").add(user_input_data)
+
+        with st.spinner("回答を生成中です..."):
+            # Generate llm response
+            response = openai.ChatCompletion.create(
+                model=MODEL_NAME,
+                messages=[
+                    {"role":data["role"], "content":data["content"]}
+                    for data in st.session_state.displayed_chat_messages
+                ]
+            )
+            assistant_output_text = response['choices'][0]['message']['content']
+
+            # Assistant
+            with st.chat_message("assistant"):
+                st.markdown(assistant_output_text)
+            assistant_output_data = {
+                "role": "assistant",
+                "content": assistant_output_text,
+                "timestamp": firestore.SERVER_TIMESTAMP
+            }
+            st.session_state.displayed_chat_messages.append(assistant_output_data)
+            st.session_state.displayed_chat_ref.collection("messages").add(assistant_output_data)
 
 
-# 定数定義
-USER_NAME = "user"
-ASSISTANT_NAME = "assistant"
-
-# OpenAIクライアントの初期化
-client = openai.ChatCompletion()
-
-def response_chatgpt(user_msg: str, past_messages: list):
-    """ChatGPTのレスポンスを取得
-
-    Args:
-        user_msg (str): ユーザーメッセージ。
-        past_messages (list): 過去のメッセージリスト（ユーザーとアシスタントの両方）。
-    """
-    # 過去のメッセージに現在のメッセージを追加
-    messages_to_send = past_messages + [{"role": "user", "content": user_msg}]
-
-    # ChatGPTにメッセージを送信し、レスポンスを取得
-    response = client.create(
-        model="gpt-4-0125-preview",
-        messages=messages_to_send,
-        stream=True,
-    )
-    return response
-
-# メッセージ履歴の初期化と初期プロンプトの設定
-if "messages" not in st.session_state:
-    initial_prompt = str(st.secrets["AppSettings"]["initial_prompt"])
-    st.session_state["messages"] = [{"role": "system", "content": initial_prompt}]
-
-# チャットログを保存したセッション情報を初期化
-if "chat_log" not in st.session_state:
-    st.session_state.chat_log = []
-
-user_msg = st.chat_input("ここにメッセージを入力")
-if user_msg:
-    # 以前のチャットログを表示
-    for chat in st.session_state.chat_log:
-        with st.chat_message(chat["name"]):
-            st.write(chat["msg"])
-
-    # 最新のメッセージを表示
-    with st.chat_message(USER_NAME):
-        st.write(user_msg)
-
-    # アシスタントのメッセージを表示
-    response = response_chatgpt(user_msg, st.session_state["messages"])
-    with st.chat_message(ASSISTANT_NAME):
-        assistant_msg = ""
-        assistant_response_area = st.empty()
-        for chunk in response:
-            if chunk.choices[0].finish_reason is not None:
-                break
-            # 回答を逐次表示
-            assistant_msg += chunk.choices[0].delta.content
-            assistant_response_area.write(assistant_msg)
-
-    # セッションにチャットログを追加
-    st.session_state["messages"].append({"role": "user", "content": user_msg})
-    st.session_state["messages"].append({"role": "assistant", "content": assistant_msg})
-    st.session_state.chat_log.append({"name": USER_NAME, "msg": user_msg})
-    st.session_state.chat_log.append({"name": ASSISTANT_NAME, "msg": assistant_msg})
+if __name__ == "__main__":
+    run()
