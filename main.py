2cb7324eb31dda3781d4fe06b195c10d709ff566
fitquick <141992941+fitquick@users.noreply.github.com>
2024-02-01 17:12:17 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import openai
 import streamlit as st
-from google.cloud import firestore
+import firestore
 
 # Streamlitの秘密管理機能を使用してOpenAI APIキーを設定
 openai.api_key = st.secrets["OpenAIAPI"]["openai_api_key"]
